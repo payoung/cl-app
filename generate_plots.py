dbda865548d9dadbafc515eb2bd8c1a5ba8445a1
payoung <paul.andy.young@gmail.com>
2014-03-13 21:27:33 -0700

    changed plots to hourly averages

--- generate_plots.py
+++ generate_plots.py
@@ -5,6 +5,8 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import os.path, datetime
+from pytz import timezone
+import pytz
 
 def get_plot(alert):
 
@@ -20,14 +22,50 @@ def get_plot(alert):
     if os.path.isfile(pname):
         plot_m_time = datetime.datetime.fromtimestamp(os.path.getmtime(pname))
         if datetime.datetime.now() - plot_m_time < datetime.timedelta(hours=1):
-            unpdate_req = False
+            update_req = False
 
     # If an update is necessary, generate a new plot
     if update_req == True:
-        days = mdates.DayLocator()
-        dayfmt = mdates.DateFormatter('%m-%d-%Y')
 
         scrapes = db_session.query(Scrape).filter_by(alert=alert).all()
+        mytz = timezone('US/Pacific')
+
+        #########################
+        # create an hourly plot #
+        #########################
+
+        hrs = range(24)
+
+        # calculate average number of new posts by hour of day
+        new_posts_by_hr = [0] * 24
+        scrape_cnt = [0] * 24
+        for scrape in scrapes:
+            scrape_cnt[scrape.dt.hour] += 1
+            new_posts_by_hr[scrape.dt.replace(tzinfo=pytz.utc).astimezone(mytz).hour] += scrape.new_posts
+
+        avg_new_posts_by_hr = [float(new_posts_by_hr[i])/float(scrape_cnt[i])
+                                for i in range(len(hrs))]     
+
+        # get the last 24 posts
+        last_24_new_posts_by_hr = [0] * 24
+        for scrape in scrapes[len(scrapes)-24:]:
+            last_24_new_posts_by_hr[scrape.dt.replace(tzinfo=pytz.utc).astimezone(mytz).hour] = scrape.new_posts
+
+        # generate plot        
+        with plt.xkcd():
+            fig, ax = plt.subplots()
+            plt.xlabel('Hour of Day')
+            plt.ylabel('# of New Posts')
+            plt.plot(hrs, avg_new_posts_by_hr, label='Overall Average')
+            plt.plot(hrs, last_24_new_posts_by_hr, label='Last 24 Hrs')
+            legen = plt.legend(loc='upper left', shadow = True)
+            plt.xlim(0, 23)
+            fig.savefig(pname)
+
+            
+    """
+        #days = mdates.DayLocator()
+        dayfmt = mdates.DateFormatter('%m-%d-%Y')
 
         dts = []
         new_posts = []
@@ -36,13 +74,24 @@ def get_plot(alert):
             dts.append(scrape.dt)
             new_posts.append(scrape.new_posts)
 
+        mpl_dts = mdates.date2num(dts)
+
         with plt.xkcd():
             fig, ax = plt.subplots()
             plt.xlabel('Date')
             plt.ylabel('New posts')
-            plt.plot(dts, new_posts)
-            ax.xaxis.set_major_locator(days)
+            plt.plot_date(mpl_dts, new_posts, linestyle='-')
+            #ax.xaxis.set_major_locator(days)
             ax.xaxis.set_major_formatter(dayfmt)
+            fig.autofmt_xdate(bottom=0.18)
             fig.savefig(pname)
+    """
 
     return pname
+
+if __name__ == "__main__":
+    alerts = db_session.query(Alert).all()
+    if alerts:
+        get_plot(alerts[2])
+    else:
+        print "No alerts in database, cannot run plot function"
