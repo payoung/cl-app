a16d401f307813f6723a81aedfa7464dc7fcf7cc
payoung <paul.andy.young@gmail.com>
2014-03-19 21:28:21 -0700

    set up alembic revision table

---
+++ alembic/versions/59314eac9d20_create_account_table.py
@@ -0,0 +1,22 @@
+"""create account table
+
+Revision ID: 59314eac9d20
+Revises: None
+Create Date: 2014-03-19 21:22:49.404926
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '59314eac9d20'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
